b88c3d422ae2a93183152bd9e2dc10f2c181cc5c
Noah Daniels <ndaniels@mac.com>
2017-01-30 11:59:26 -0500

    Finished writing lab

--- stats.cpp
+++ stats.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <cstdlib>
-
+ 
 int get_max(int list[], int length) {
-	int guess = 0;
-	for (int i = 0; i < length; i++) {
-		int candidate = list[i];
-		if (candidate > guess) guess = candidate;
-	}
-	return guess;
-}
-
-int main(int argc, char* argv[]) {
-	int numbers[argc-1];
-    for (int i = 0; i < argc-1; i++) {
-    	numbers[i] = atoi(argv[i]);
+    int guess = 0;
+    for (int i = 0; i < length; i++) {
+        int candidate = list[i];
+        if (candidate > guess) {guess = candidate;}
     }
-    int max = get_max(numbers, argc-1);
+    return guess;
+}
+ 
+int main(int argc, char* argv[]) {
+    int numbers[argc];
+    for (int i = 1; i < argc; i++) {
+        numbers[i] = atoi(argv[i]);
+    }
+    int max = get_max(numbers, argc);
     std::cout << max;
     std::cout << std::endl;
     return 0;
 }
-
